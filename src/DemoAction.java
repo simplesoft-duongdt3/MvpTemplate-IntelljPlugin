d1760afc36602ae5ba99b3af75bf4f571c9ed766
doanthanhduong <duongd@nowpos.vn>
2018-12-25 09:28:35 +0700

    Initial commit

---
+++ src/DemoAction.java
@@ -0,0 +1,15 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class DemoAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+}
